6c7b48e4849f6074aa64b8b411fb1adf2b0d62d3
steijehillewaert <info@steije.be>
2018-01-23 15:04:40 +0100

    Added content to index page

--- src/view/events/index.php
+++ src/view/events/index.php
@@ -1,22 +1,44 @@
-<section>
-  <h1>Events</h1>
-  <?php foreach($events as $event): ?>
-    <article>
-      <header><h2><?php echo $event['title']; ?></h2></header>
-      <dl>
-        <dt>code</dt><dd><?php echo $event['code'];?></dd>
-        <dt>content</dt><dd><?php echo $event['content'];?></dd>
-        <dt>practical</dt><dd><?php echo $event['practical'];?></dd>
-        <dt>location</dt><dd><?php echo $event['location'];?></dd>
-        <dt>address</dt><dd><?php echo $event['address'];?></dd>
-        <dt>postal</dt><dd><?php echo $event['postal'];?></dd>
-        <dt>city</dt><dd><?php echo $event['city'];?></dd>
-        <dt>start</dt><dd><?php echo $event['start'];?></dd>
-        <dt>end</dt><dd><?php echo $event['end'];?></dd>
-        <dt>link</dt><dd><?php echo $event['link'];?></dd>
-        <dt>organisers</dt><dd><ul><?php foreach($event['organisers'] as $organiser): ?><li><?php echo $organiser['name'];?></li><?php endforeach;?></ul></dd>
-        <dt>tags</dt><dd><ul><?php foreach($event['tags'] as $tag): ?><li><?php echo $tag['tag'];?></li><?php endforeach;?></ul></dd>
-      </dl>
-    </article>
-  <? endforeach;?>
+<section class="hero">
+  <h1>Week van de <br>
+  mobiliteit</h1>
+  <h1>16 tot 22 september 2018</h1>
 </section>
+
+<main>
+  <h2>Uitgelichte activeiteiten</h2>
+  <section>
+    <?php foreach($events as $event): ?>
+      <article>
+        <?php echo $event['title']; ?>
+        <?php echo $event['city'];?>
+        <?php echo date('d/m', strtotime($event['start']));?>
+      </article>
+    <? endforeach;?>
+    <a href="#">Volledige programma</a>
+  </section>
+  <section>
+    <img src="#" alt="">
+    <h2>Wat is de week van de mobiliteit?</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium neque ante, id volutpat metus pulvinar non. Fusce quis magna fringilla, volutpat est eget, congue tortor. Phasellus ac pellentesque arcu, feugiat ultrices nisl. Suspendisse potenti. </p>
+    <a href="#">Ontdek de week van de mobiliteit</a>
+  </section>
+  <section>
+    <div class="blue">
+      <h3>Dien je actie in voor <br>
+        de week van de mobiliteit 2018</h3>
+    </div>
+    <div class="green">
+      <h3>Nieuwsbrief</h3>
+      <p>Blijf op de hoogte van de laatste details en nieuwtjes</p>
+      <input type="text" name="" value="">
+      <input type="submit" name="Verzenden" value="nieuwsbrief">
+    </div>
+  </section>
+  <section>
+    <h2>Partners</h2>
+    <img src="#" alt="">
+    <img src="#" alt="">
+    <img src="#" alt="">
+    <img src="#" alt="">
+  </section>
+</main>
