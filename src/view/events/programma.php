6542503c5b13f8e8cfdeebb39a5817781864a774
steijehillewaert <info@steije.be>
2018-01-24 14:40:00 +0100

    Added event data and added classes for styling

--- src/view/events/programma.php
+++ src/view/events/programma.php
@@ -1,4 +1,7 @@
-<section class="programma">
+<section class="programma container">
+  <div class="container">
+    <h1>Programma</h1>
+    <form class="" action="?page=programma" method="post">
     <select name='date' id='date'>
       <option disabled selected>Kies een datum</option>
       <?php foreach($events as $event): ?>
@@ -6,17 +9,19 @@
         <option><?php echo date('d/m/Y', strtotime($event['end']));?></option>
       <?php endforeach ?>
     </select>
-    <form class="" action="?page=programma" method="post">
       <input type="text" name="postal" placeholder="Postcode ..." id="search">
-      <input type="submit" name="action" value="zoek">
+      <input type="submit" name="action" value=" " class="search">
   </form>
-
+  </div>
+</section>
+  <section class="events container">
   <?php foreach($events as $event): ?>
-    <article style="background:url(assets/events/<?php echo $event['code'];?>/thumb.jpg)">
-      <div class="">
-        <h2><?php echo $event['title']; ?></h2>
+    <article class="event" style="background:url(assets/events/<?php echo $event['code'];?>/thumb.jpg)">
+      <h2 class="date_event"><?php echo date('d/m', strtotime($event['start']));?></h2>
+      <div class="event_container">
+        <h2 class="event_title"><?php echo $event['title']; ?></h2>
         <a href="index.php?page=detail&amp;id=<?php echo $event['id']; ?>">Meer info</a>
       </div>
     </article>
   <? endforeach;?>
-</section>
+  </section>
