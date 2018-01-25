e95ff2f447c3a2b4b040b8069be8b3fd844b1a89
steijehillewaert <info@steije.be>
2018-01-25 23:20:23 +0100

    First ty for AJAX search

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,32 @@
+const $searchInput = document.querySelector(`.search_field`);
+const $events = document.querySelector(`.events`);
+
 const init = () => {
-  // console.log(`Sup Devine`);
+  $searchInput.addEventListener(`input`, handleInputSearch);
+};
+
+const handleInputSearch = () => {
+  const q = $searchInput.value.trim();
+  console.log(q);
+  if (q.length > 0) {
+    fetch(`index.php?query=${q}&page=programma`, {
+      headers: new Headers({
+        Accept: `application/json`
+      })
+    })
+      .then(r => r.json())
+      .then(data => parse(data));
+  }
+};
+
+const parse = results => {
+  $events.innerHTML = results
+    .map(event => createEvent(event))
+    .join(``);
+};
+
+const createEvent = event => {
+  console.log(event);
 };
 
 init();
