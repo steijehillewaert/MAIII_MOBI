fc013dbfb2d52726f786ee1fab7dde64883f9412
steijehillewaert <info@steije.be>
2018-01-28 12:32:44 +0100

    Added Expire date for page speed

--- src/view/layout.php
+++ src/view/layout.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="Expires" content="30">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Week van de mobiliteit</title>
     <script>
@@ -22,7 +23,7 @@
     <?php echo $css;?>
   </head>
   <body>
-      <header>
+      <header class="nav">
         <div class="container header">
           <nav>
             <ul>
