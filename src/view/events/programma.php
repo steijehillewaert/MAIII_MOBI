9de6b6de3d1fa8980d6629230fbb372fcbaa9066
steijehillewaert <info@steije.be>
2018-01-28 21:31:10 +0100

    Fixed css without JS

--- src/view/events/programma.php
+++ src/view/events/programma.php
@@ -14,9 +14,11 @@
         <option value="2018-09-22" <?php if(!empty($_GET['date']) && $_GET['date'] == '2018-09-22') echo 'selected';?>>22/09/2018</option>
         <option value="2018-09-24" <?php if(!empty($_GET['date']) && $_GET['date'] == '2018-09-24') echo 'selected';?>>24/09/2018</option>
       </select>
-      <input type="text" name="postal" placeholder="Gemeente ..." <?php if(!empty($_GET['postal'])) echo 'value="' . $_GET['postal'] . '"';?>>
+      <div class="mobile-input-flex">
+        <input type="text" name="postal" placeholder="Gemeente ..." <?php if(!empty($_GET['postal'])) echo 'value="' . $_GET['postal'] . '"';?>>
+        <input type="submit" class="search noJS" value=" ">
+      </div>
       <input type="hidden" name="page" value="programma">
-      <input type="submit" class="search noJS" value=" ">
     </form>
     <input type="submit" name="reset" class="reset hide" value="Reset Filter">
     <a href="?page=programma" class="reset noJS">Reset Filter</a>
